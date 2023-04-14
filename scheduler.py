c30328376f5c25d6592e8103212718b05994a529
Tiago Vargas <tiago.ce10@outlook.com>
2023-04-13 21:34:23 -0300

    begin executing first process in queue

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,11 @@
 class RoundRobinScheduler:
     def __init__(self, quantum: int, context_switching_duration: int):
         self.clock = 0
+        self.queue = None
+        self.current_process = None
 
     def simulate_scheduling(self):
         self.clock += 5
+
+    def initialize(self):
+        self.current_process = self.queue[0]
--- test_scheduler.py
+++ test_scheduler.py
@@ -13,3 +13,14 @@ class TestRoundRobinScheduler:
                 scheduler.simulate_scheduling()
 
                 assert scheduler.clock == 5
+
+    class TestMoreProcess:
+        def test_first_process_in_queue_to_be_the_one_to_execute_now(self):
+            scheduler = RoundRobinScheduler(quantum=20, context_switching_duration=1)
+            process_1 = Process(burst_duration=50)
+            process_2 = Process(burst_duration=50)
+            scheduler.queue = [process_1, process_2]
+
+            scheduler.initialize()
+
+            assert scheduler.current_process == process_1
