17af3c2c3bd7b1440e8c9369af7083b54e51d414
Tiago Vargas <tiago.ce10@outlook.com>
2023-04-13 23:11:53 -0300

    implement `Process.__repr__` for debugging

--- process.py
+++ process.py
@@ -1,4 +1,11 @@
 class Process:
+    pid = 1
+
     def __init__(self, burst_duration: int):
+        self.pid = Process.pid
+        Process.pid += 1
         self.is_finished = False
         self.remaining_duration = burst_duration
+
+    def __repr__(self):
+        return f'Process #{self.pid}'
